ce54ca9024f48547ee12b6182662fedccc2ba7fc
Narayan2208 <narayandutta2208@gmail.com>
2023-05-16 02:34:31 +0530

    cocktail showing

--- cocktailsearch/src/component/CocktailList.jsx
+++ cocktailsearch/src/component/CocktailList.jsx
@@ -1,47 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCocktail } from "../redux/action";
-import { Spinner, Text } from '@chakra-ui/react'
+import { Box, Spinner, Text, Grid, GridItem } from "@chakra-ui/react";
 const CocktailList = () => {
   let dispatch = useDispatch();
   const { cocktails, loading } = useSelector((state) => ({ ...state.data }));
-  const [modifiedCocktail, setModifiedCocktail] = useState([])
+  const [modifiedCocktail, setModifiedCocktail] = useState([]);
 
   useEffect(() => {
     dispatch(fetchCocktail());
   }, []);
 
   useEffect(() => {
-   if(cocktails){
-    const newCocktails = cocktails.map((item) =>{
-      const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item;
-      return {
-        id: idDrink,
-        name : strDrink,
-        image : strDrinkThumb,
-        info : strAlcoholic,
-        glass : strGlass
-      }
-
-    });
-    setModifiedCocktail(newCocktails);
-   }else{
-    setModifiedCocktail([]);
-   }
+    if (cocktails) {
+      const newCocktails = cocktails.map((item) => {
+        const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+          item;
+        return {
+          id: idDrink,
+          name: strDrink,
+          image: strDrinkThumb,
+          info: strAlcoholic,
+          glass: strGlass,
+        };
+      });
+      setModifiedCocktail(newCocktails);
+    } else {
+      setModifiedCocktail([]);
+    }
   }, []);
 
-  if(loading){
-    return (
-      <Spinner color='red.500' />
-    )
+  if (loading) {
+    return <Spinner color="red.500" />;
   }
 
-  if(!cocktails){
-    return <Text>Mo Cocktails matched</Text>
+  if (!cocktails) {
+    return <Text>Mo Cocktails matched</Text>;
   }
   return (
     <>
       <h2>CocktailList</h2>
+      <Box>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          {
+            modifiedCocktail.map((item) => {
+                          return (
+                            <GridItem key={item.id}>
+                              <img src={item.image} alt={item.name} />
+                              <p>{item.name}</p>
+                              <p>{item.info}</p>
+                              <p>{item.glass}</p>
+                            </GridItem>
+                          );
+                        })
+          }
+        </Grid>
+      </Box>
     </>
   );
 };

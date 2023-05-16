3963a953c501645ba17329aa4f35e0e0904f5834
Narayan2208 <narayandutta2208@gmail.com>
2023-05-16 20:44:38 +0530

    error fixed

--- cocktailsearch/src/pages/SingleCocktail.jsx
+++ cocktailsearch/src/pages/SingleCocktail.jsx
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { fetchSingleCocktail } from '../redux/action'
-import {useSelector, useDispatch} from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { Text } from '@chakra-ui/react'
+import { Button, Spinner } from "@chakra-ui/react";
+
 const SingleCocktail = () => {
-  const {cocktail, loading} = useSelector ((state) => ({...state.data}));
-  console.log(cocktail.length, "hi");
-  const {modifiedCocktail, setModifiedCocktail} = useState(null)
-  const {id} = useParams();
+  const { cocktail, loading } = useSelector((state) => ({ ...state.data }));
+  const [modifiedCocktail, setModifiedCocktail] = useState(null);
+  const { id } = useParams();
   let dispatch = useDispatch();
 
-  useDispatch(()=>{
-    dispatch(fetchSingleCocktail(id))
-  }, [id])
+  useEffect(() => {
+    dispatch(fetchSingleCocktail(id));
+  }, [id]);
 
-  useEffect(()=>{
-    if(cocktail.length > 0){
+  useEffect(() => {
+    if (cocktail && cocktail.length > 0) {
       const {
-        strDrink : name,
-        strDrinkThumb : image,
-        strAlcoholic : info,
-        strCategory : category,
-        strGlass : glass,
-        strInstructions : instructions,
+        strDrink: name,
+        strDrinkThumb: image,
+        strAlcoholic: info,
+        strCategory: category,
+        strGlass: glass,
+        strInstructions: instructions,
         strIngredient1,
         strIngredient2,
         strIngredient3,
         strIngredient4,
         strIngredient5,
-
       } = cocktail[0];
       const ingredients = {
         strIngredient1,
@@ -43,20 +43,33 @@ const SingleCocktail = () => {
         info,
         category,
         glass,
-        instructions
-      }
-      setModifiedCocktail(newCocktail)
-    }else{
-      setModifiedCocktail(null)
+        instructions,
+        ingredients,
+      };
+      setModifiedCocktail(newCocktail);
+    } else {
+      setModifiedCocktail(null);
     }
-  }, [id, cocktail])
+  }, [id, cocktail]);
 
-  if(!modifiedCocktail){
-    return <Text>No Cocktail to display</Text>
+  if (!modifiedCocktail) {
+    return <Text>No Cocktail to display</Text>;
+  } else {
+    const { name, image, category, info, glass, instructions, ingredients } = modifiedCocktail;
+    return (
+      <>
+        {loading ? (
+          <Spinner color="red.500" />
+        ) : (
+          <section>
+            <Link to="/">
+              <Button>Go back</Button>
+            </Link>
+          </section>
+        )}
+      </>
+    );
   }
-  return (
-    <div>SingleCocktail</div>
-  )
 }
 
-export default SingleCocktail
+export default SingleCocktail;

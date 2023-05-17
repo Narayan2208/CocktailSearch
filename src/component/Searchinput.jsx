338dd8679f9728925398ad238daec200349a570b
Narayan2208 <narayandutta2208@gmail.com>
2023-05-17 17:45:50 +0530

    input styling fixed

--- src/component/Searchinput.jsx
+++ src/component/Searchinput.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchSearchCocktail } from '../redux/action';
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchSearchCocktail } from "../redux/action";
+import { Text } from "@chakra-ui/react";
+import  "./SeachInput.css"
 const SearchInput = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
-  
+
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
     dispatch(fetchSearchCocktail(searchTerm));
@@ -18,13 +19,31 @@ const SearchInput = () => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <div style={{ display:"flex"}}>  
-          <label htmlFor='name'>Search Cocktail </label>
-          <div style={{border : "2px solid black" , marginLeft : "20px"}}>
-          <input type='text' name='name' id='name' value={searchTerm} onChange={handleChange} />
+        <div style={{ display: "flex" }}>
+          <Text
+            fontSize="25px"
+            fontWeight={"bolder"}
+            fontFamily={"mono"}
+            color={"white"}
+            text-shadow="2px 7px 5px rgba(0,0,0,0.3), 
+    0px -4px 10px rgba(255,255,255,0.3)"
+            background={"black"}
+            letter-spacing= "5px"
+          >
+            Search Cocktail
+          </Text>
+          <div style={{  marginLeft: "20px" }}>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={searchTerm}
+              onChange={handleChange}
+              border= "1px solid black"
+              width={"100%"}
+            />
           </div>
         </div>
-       
       </form>
     </>
   );
